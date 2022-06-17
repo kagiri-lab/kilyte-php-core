4ce1212de71f455721212229e1678da0da063dd2
Kagiri <kagiri@kagiri.com>
2022-06-17 15:09:03 +0300

    Vendor updates

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bdd1eb11c9c2d22b06c45923e44ca908d721089a',
+    'name' => 'kilyte/kilyte-php-core',
+  ),
+  'versions' => 
+  array (
+    'kilyte/kilyte-php-core' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bdd1eb11c9c2d22b06c45923e44ca908d721089a',
+    ),
+  ),
+);
