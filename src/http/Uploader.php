fd09fd9a924dadd768eeebca7beed27dff1cf96e
kagiri <kagiri@sharksight.co.ke>
2022-11-15 22:08:05 +0300

    uploader update

--- src/http/Uploader.php
+++ src/http/Uploader.php
@@ -9,13 +9,13 @@ class Uploader
 {
     public static function upload()
     {
+        self::validateFile();
         $location = self::storageDirectory();
         $filename = $_FILES["file"]["name"];
         if (is_array($filename))
             $filename = $filename[0];
         $filetype = $_FILES["file"]["type"];
         $filesize = $_FILES["file"]["size"];
-        $maxsize = 1024 * 1024 * 1024;
 
         $ext = pathinfo($filename, PATHINFO_EXTENSION);
         $title = basename($filename, "." . $ext);
@@ -25,7 +25,6 @@ class Uploader
             "name" => $filename,
             "type" => $filetype,
             "size" => $filesize,
-            "maxsize" => $maxsize,
             "location" => $location,
             "ext" => $ext
         );
@@ -65,5 +64,67 @@ class Uploader
             }
         }
         return $location;
+    }
+
+    static function allowedExtensions()
+    {
+
+        $allowed = array(
+            "jpg" => "image/jpg",
+            "jpeg" => "image/jpeg",
+            "gif" => "image/gif",
+            "png" => "image/png",
+            "mov" => "video/mov",
+            "mp4" => "video/mp4",
+            "3gp" => "video/3gp",
+            "ogg" => "video/ogg",
+            "flv" => "video/x-flv",
+            "mov" => "video/quicktime",
+            "avi" => "video/x-msvideo",
+            "wmv" => "video/x-msvideo",
+            "m3u8" => "application/x-mpegURL",
+            "ts" => "video/MP2T",
+            "mp3" => "audio/mpeg",
+            "wav" => "audio/mav",
+            "pdf" => "application/pdf",
+            "doc" => "application/doc",
+            "docx" => "application/docx",
+            "docx" => "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            "rtf" => "application/rtf",
+            "txt" => "text/plain",
+            "odf" => "application/odf",
+            "xls" => "application/xls",
+            "xlsx" => "application/xlsx",
+            "xlsx" => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "msword" => "application/msword",
+            "zip" => "application/zip",
+            "rar" => "application/x-rar-compressed",
+            "octet-stream" => "application/octet-stream"
+        );
+
+        return $allowed;
+    }
+
+    static function validateFile()
+    {
+        if (empty($_FILES["file"]))
+            throw new Exception('File not set');
+
+        if (!empty($_FILES["file"]["error"]))
+            switch ($_FILES["file"]["error"]) {
+                case 4:
+                    throw new Exception('No file was uploaded');
+                    break;
+                default:
+                    throw new Exception('File upload error');
+                    break;
+            }
+
+        $filename = $_FILES["file"]["name"];
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        if (!empty($ext))
+            $ext = strtolower($ext);
+        if (!array_key_exists($ext, self::allowedExtensions()))
+            throw new Exception('Invalid file format.');
     }
 }
