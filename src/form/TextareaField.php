4b94694cbf80d9010a9368fe8185ef79686fadac
kagiri <kagiri@sharksight.co.ke>
2023-04-10 21:08:48 +0300

    text area field update

--- src/form/TextareaField.php
+++ src/form/TextareaField.php
@@ -6,8 +6,13 @@ class TextareaField extends BaseField
 {
     public function renderInput()
     {
-        return sprintf('<textarea class="form-control%s" name="%s">%s</textarea>',
-             $this->model->hasError($this->attribute) ? ' is-invalid' : '',
+        $attributes = [];
+        foreach ($this->options as $key => $value) {
+            $attributes[] = "$key=\"$value\"";
+        }
+        return sprintf(
+            '<textarea class="form-control%s" name="%s">%s</textarea>',
+            $this->model->hasError($this->attribute) ? ' is-invalid' : '',
             $this->attribute,
             $this->model->{$this->attribute},
         );
